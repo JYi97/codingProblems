dfa32de6c47186dc896ca1049fccc1f2f67e4d38
justin <jyi@ucsb.edu>
2022-08-19 14:48:04 -0700

    structy isPrime js

---
+++ structy/javascript/isPrime.js
@@ -0,0 +1,9 @@
+const isPrime = (n) => {
+    if (n === 1) return false
+    for (let i = 2; i < n - 1; i++) {
+        if (n % i === 0) return false
+    }
+    return true
+  };
+
+console.log(isPrime(1))
